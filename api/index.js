e1a21358c91cd0b3e3ef1c390a5ddcf6cb8a3083
sourabh <sourabhsinghgsc@gmail.com>
2026-05-27 22:43:42 +0530

    changes made

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/test', testRoutes);
 app.use('/api/result', resultRoutes);
 // test route
-app.get('/', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
